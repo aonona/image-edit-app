eb534c41eeae58ca3d652598778005772ce2ee30
aonona <124593461+aonona@users.noreply.github.com>
2026-09-12 08:22:53 +0900

    Switch PixelSlot to clean v0.8 renderer

--- pixelslot/app/src/main/java/com/aonon/pixelslot/MainActivity.java
+++ pixelslot/app/src/main/java/com/aonon/pixelslot/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends Activity {
         Window window = getWindow();
         window.setStatusBarColor(Color.rgb(33, 18, 52));
         window.setNavigationBarColor(Color.rgb(22, 12, 38));
-        setContentView(new PixelSlotV7View(this));
+        setContentView(new PixelSlotV8View(this));
     }
 }
