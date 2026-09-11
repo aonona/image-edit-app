329758380e5541fa352a7a757d4ce3aa4b6ddbb9
aonona <124593461+aonona@users.noreply.github.com>
2026-09-12 07:44:04 +0900

    Switch PixelSlot to v0.7 visual correction overlay

--- pixelslot/app/src/main/java/com/aonon/pixelslot/MainActivity.java
+++ pixelslot/app/src/main/java/com/aonon/pixelslot/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends Activity {
         Window window = getWindow();
         window.setStatusBarColor(Color.rgb(33, 18, 52));
         window.setNavigationBarColor(Color.rgb(22, 12, 38));
-        setContentView(new PixelSlotV6OverlayView(this));
+        setContentView(new PixelSlotV7View(this));
     }
 }
