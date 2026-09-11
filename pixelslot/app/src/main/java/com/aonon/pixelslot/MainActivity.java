9b750c66ad3515350be9a4f3d3027f3013f84dc9
aonona <124593461+aonona@users.noreply.github.com>
2026-09-11 22:13:55 +0900

    Add PixelSlot main activity

---
+++ pixelslot/app/src/main/java/com/aonon/pixelslot/MainActivity.java
@@ -0,0 +1,17 @@
+package com.aonon.pixelslot;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setStatusBarColor(Color.rgb(33, 18, 52));
+        window.setNavigationBarColor(Color.rgb(22, 12, 38));
+        setContentView(new PixelSlotView(this));
+    }
+}
